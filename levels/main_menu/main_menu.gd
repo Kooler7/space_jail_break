c451679129400528a9329975d9759009bb92c1b3
Kooler7 <holgcool@bk.ru>
2025-09-03 15:40:57 +0300

    MainMenu InProgress

--- levels/main_menu/main_menu.gd
+++ levels/main_menu/main_menu.gd
@@ -2,8 +2,17 @@
 extends Node2D
 
 
+@export var main_screen_buttons_group : ButtonGroup
+@onready var version : Label = $VersionNumber
+@onready var accept_button : TextureButton = $MainMenuCallBtn
 
-@onready var version : Label = $MainBG/VersionNumber
+var buttons_actions : Dictionary = {
+	"MainMenuResumeBtn" : "on_resume_btn_pressed",
+	"MainMenuStartBtn" : "on_start_btn_pressed",
+	"MainMenuSettingsBtn" : "on_settings_btn_pressed",
+	"MainMenuExitBtn" : "on_exit_button_pressed"
+}
+var current_button_name : String =""
 @onready var camera : Camera2D = Settings.camera
 
   
@@ -17,10 +26,29 @@ enum CameraPositions {
 	SETTINGS
 }
 var current_cp : CameraPositions = CameraPositions.MAIN
+var main_screen_buttons : Array
+
 
 
 func _ready() -> void:
+	main_screen_buttons = main_screen_buttons_group.get_buttons()
+	main_screen_buttons.sort()
+	for button in main_screen_buttons:
+		button.pressed.connect(on_main_screen_button_pressed.bind(button))
+	
+	accept_button.pressed.connect(on_accept_button_pressed)
+	
 	version.text = "Ver. " + ProjectSettings.get_setting("application/config/version")
+
+
+
+func on_main_screen_button_pressed(button : TextureButton)-> void:
+	current_button_name = button.name
+
+
+func on_accept_button_pressed() ->void:
+	call(buttons_actions[current_button_name])
+
 
 
 func check_camera_position(new_position) -> void:
@@ -37,10 +65,15 @@ func _on_back_btn_pressed() -> void:
 	check_camera_position(CameraPositions.MAIN)
 
 
-func _on_settings_btn_pressed() -> void:
+func on_settings_btn_pressed() -> void:
 	check_camera_position(CameraPositions.SETTINGS)
 
-
-func _on_start_btn_pressed() -> void:
+func on_start_btn_pressed() -> void:
 	Globals.main.loading_level_path = "res://levels/chapter_1/chapter_1.tscn"
 	Globals.main.start_loading()
+
+func on_resume_btn_pressed() -> void:
+	print("Отказано!")
+
+func  on_exit_button_pressed() -> void:
+	get_tree().quit()
