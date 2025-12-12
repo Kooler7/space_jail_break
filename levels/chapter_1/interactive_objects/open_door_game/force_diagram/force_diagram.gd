2d63e9c1050c36fa51de069e9854fa6ff21f1e30
Kooler7 <holgcool@bk.ru>
2025-12-12 21:04:59 +0300

    Update DoorGame

--- levels/chapter_1/interactive_objects/open_door_game/force_diagram/force_diagram.gd
+++ levels/chapter_1/interactive_objects/open_door_game/force_diagram/force_diagram.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal pipe_placed
+signal pipe_lost_place
 
 const PIPE_TO_IDLE_SPEED : float = 10
 const PIPE_ROTATION_SPEED : float = 2
@@ -11,25 +13,41 @@ const MAX_DIAGRAM_ANGLE : float = 180
 @onready var diagram_pipe : Sprite2D = $DiagramPipe
 
 var is_pipe_placed = false
+var delta_angle : float
+var is_started = false
+
+
+
 
 
 func _process(delta: float) -> void:
-	turn_pipe_to_idle(delta)
+	if is_started:
+		delta_angle = abs(diagram_pipe.rotation_degrees - diagram.rotation_degrees)
+		if delta_angle < 10:
+			if is_pipe_placed == false:
+				is_pipe_placed = true
+				emit_signal("pipe_placed")
+		elif delta_angle > 10:
+			if is_pipe_placed == true:
+				is_pipe_placed = false
+				emit_signal("pipe_lost_place")
+		turn_pipe_to_idle(delta)
 
 
 
 
 func calculate_new_diagram_angle() -> void:
-	diagram_timer.wait_time = randi_range(1, 3)
-	diagram_timer.start()
-	await  diagram_timer.timeout
-	var take_angle = randi_range(0, 1)
-	if take_angle == 1:
-		var angle = randi_range(MIN_DIAGRAM_ANGLE, MAX_DIAGRAM_ANGLE)
-		rotate_diagram(angle)
-		calculate_new_diagram_angle()
-	elif take_angle == 0:
-		calculate_new_diagram_angle()
+	if is_started:
+		diagram_timer.wait_time = randi_range(1, 3)
+		diagram_timer.start()
+		await  diagram_timer.timeout
+		var take_angle = randi_range(0, 1)
+		if take_angle == 1:
+			var angle = randi_range(MIN_DIAGRAM_ANGLE, MAX_DIAGRAM_ANGLE)
+			rotate_diagram(angle)
+			calculate_new_diagram_angle()
+		elif take_angle == 0:
+			calculate_new_diagram_angle()
 
 
 func rotate_diagram(new_angle : float) -> void:
@@ -40,21 +58,23 @@ func rotate_diagram(new_angle : float) -> void:
 
 
 func turn_pipe_to_idle(delta : float) -> void:
-	if diagram_pipe.rotation_degrees > 90:
-		diagram_pipe.rotation_degrees -= PIPE_TO_IDLE_SPEED * delta
-		return
-	if diagram_pipe.rotation_degrees < 90:
-		diagram_pipe.rotation_degrees += PIPE_TO_IDLE_SPEED * delta
-		return
+	if is_started:
+		if diagram_pipe.rotation_degrees > 90:
+			diagram_pipe.rotation_degrees -= PIPE_TO_IDLE_SPEED * delta
+			return
+		if diagram_pipe.rotation_degrees < 90:
+			diagram_pipe.rotation_degrees += PIPE_TO_IDLE_SPEED * delta
+			return
 
 
 
 func  _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			diagram_pipe.rotation_degrees -= PIPE_ROTATION_SPEED
+	if is_started:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				diagram_pipe.rotation_degrees -= PIPE_ROTATION_SPEED
+				return
+			if event.button_index == MOUSE_BUTTON_RIGHT:
+				diagram_pipe.rotation_degrees += PIPE_ROTATION_SPEED
+				return
 			return
-		if event.button_index == MOUSE_BUTTON_RIGHT:
-			diagram_pipe.rotation_degrees += PIPE_ROTATION_SPEED
-			return
-		return
--- levels/chapter_1/interactive_objects/open_door_game/open_door_game.gd
+++ levels/chapter_1/interactive_objects/open_door_game/open_door_game.gd
@@ -1,110 +1,90 @@
 extends Node2D
 
-#signal pipe_angle_reached
+
+const DOOR_DAMAGE_MULTIPLIER = 0.8
+const PipeDiagram = preload("res://levels/chapter_1/interactive_objects/open_door_game/force_diagram/force_diagram.gd")
+
+enum ActionStates {
+	STOPPED,
+	STARTED,
+	FINISHED,
+	PLAYER_WON
+}
+var current_action_state : ActionStates = ActionStates.STOPPED
+
 
 @onready var pipe : AnimatedSprite2D = $Pipe
 @onready var door : Sprite2D = $Door
 @onready var death_timer : Timer = $DeathTimer
 @onready var shake_door : AnimationPlayer = $AnimationPlayer
-@onready var ceiling_damage : TextureProgressBar = $Stage1Items/CeilingBar
+@onready var ceiling_damage : TextureProgressBar = $CeilingDamage
+@onready var door_damage : TextureProgressBar = $DoorDamage
+@onready var diagram : PipeDiagram = $ForceDiagram
 
 
+func _ready() -> void:
+	check_action_state(ActionStates.STARTED)
 
 
-const PIPE_TO_IDLE_SPEED : float = 10
-const PIPE_ROTATION_SPEED : float = 2
-const MIN_DIAGRAM_ANGLE : float = 0
-const MAX_DIAGRAM_ANGLE : float = 180
-
-#enum GameStages {
-	#STAGE_1,
-	#FINISHED,
-	#LOOSE
-#}
-#var current_game_stage : GameStages 
-#@export var new_stage : GameStages = GameStages.STAGE_1
-#
-#var is_pipe_in_position : bool = false
-#
-#func _ready() -> void:
-	##print(current_game_stage)
-	##print(new_stage)
-	#update_game_stage(new_stage)
-	#death_timer.start()
-	#
-#
-#
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#
-	#if current_game_stage != new_stage:
-		#update_game_stage(new_stage)
-	#
-	#if current_game_stage == GameStages.STAGE_1:
-		#turn_pipe_to_idle(delta)
-		#scene_pipe_animation()
-		#ceiling_damage.value = death_timer.time_left
-		#
-#
-#
-#func update_game_stage(new_stage) -> void:
-	#match new_stage:
-		#GameStages.STAGE_1:
-			#stage_1_items.show()
-			#current_game_stage = GameStages.STAGE_1
-			#diagram.rotation_degrees = randi_range(MIN_DIAGRAM_ANGLE, MAX_DIAGRAM_ANGLE)
-			#calculate_new_diagram_angle()
-		#GameStages.FINISHED:
-			#pass
-		#GameStages.LOOSE:
-			#pass
-#
-#func scene_pipe_animation() -> void:
-	#if abs(diagram_pipe.rotation_degrees - diagram.rotation_degrees) < 10:
-		#if pipe.frame_progress < 1:
-			#pipe.play()
-		#shake_door.play("shake_door")
-	#elif abs(diagram_pipe.rotation_degrees - diagram.rotation_degrees) > 10:
-		#if pipe.frame!= 0:
-			#pipe.play_backwards()
-			#shake_door.stop()
-#
-#func calculate_new_diagram_angle() -> void:
-	#diagram_timer.wait_time = randi_range(1, 3)
-	#diagram_timer.start()
-	#await  diagram_timer.timeout
-	#var take_angle = randi_range(0, 1)
-	#if take_angle == 1:
-		#var angle = randi_range(MIN_DIAGRAM_ANGLE, MAX_DIAGRAM_ANGLE)
-		#rotate_diagram(angle)
-		#calculate_new_diagram_angle()
-	#elif take_angle == 0:
-		#calculate_new_diagram_angle()
-#
-#func rotate_diagram(new_angle : float) -> void:
-	#var duration : float = abs(new_angle - diagram.rotation_degrees) * 0.01
-	#var rotate_tween = create_tween()
-	#rotate_tween.tween_property(diagram, "rotation_degrees", new_angle, duration)
-	#rotate_tween.play()
-#
-#func turn_pipe_to_idle(delta : float) -> void:
-	#if current_game_stage == GameStages.STAGE_1:
-		#if diagram_pipe.rotation_degrees > 90:
-			#diagram_pipe.rotation_degrees -= PIPE_TO_IDLE_SPEED * delta
-			#return
-		#if diagram_pipe.rotation_degrees < 90:
-			#diagram_pipe.rotation_degrees += PIPE_TO_IDLE_SPEED * delta
-			#return
-	#return
+func _process(delta: float) -> void:
+	if current_action_state == ActionStates.STARTED:
+		ceiling_damage.value = death_timer.time_left
+		
+		if diagram.is_pipe_placed:
+			door_damage.step = DOOR_DAMAGE_MULTIPLIER * delta
+			door_damage.value = door_damage.value - door_damage.step
+			print(door_damage.value)
+		
+		if door_damage.value <= door_damage.min_value:
+			check_action_state(ActionStates.PLAYER_WON)
+			print("PLAYER_WINS")
+
+
+
+
+func check_action_state (new_state : ActionStates):
+	if new_state != current_action_state:
+		match new_state:
+			ActionStates.STOPPED:
+				current_action_state = ActionStates.STOPPED
+			ActionStates.STARTED:
+				diagram.is_started = true
+				death_timer.start()
+				diagram.calculate_new_diagram_angle()
+				current_action_state = ActionStates.STARTED
+			ActionStates.FINISHED:
+				diagram.is_started = false
+				diagram.is_pipe_placed = false
+				death_timer.stop()
+				shake_door.stop()
+				diagram.diagram_timer.stop()
+				print("DEAAAD!!!!")
+				current_action_state = ActionStates.FINISHED
+			ActionStates.PLAYER_WON:
+				diagram.is_started = false
+				diagram.is_pipe_placed = false
+				diagram.diagram_timer.stop()
+				death_timer.stop()
+				shake_door.stop()
+				door.hide()
+				current_action_state = ActionStates.PLAYER_WON
 
 
 
 
 
+func _on_death_timer_timeout() -> void:
+	check_action_state(ActionStates.FINISHED)
 
 
-func on_game_started() -> void:
-	pass
+func _on_force_diagram_pipe_placed() -> void:
+	if pipe.frame_progress < 1:
+		pipe.play()
+	shake_door.play("shake_door")
 
-func on_game_finished() -> void:
-	pass
+
+func _on_force_diagram_pipe_lost_place() -> void:
+	if pipe.frame!= 0:
+		pipe.play_backwards()
+		shake_door.stop()
